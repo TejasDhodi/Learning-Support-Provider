530b0600636cb26dd79238de7a337598adcde2f4
Tejas Dhodi <tejas.dhodi@in10media.com>
2025-06-13 14:59:23 +0530

    remove unwanted code

--- Learning_Support_Provider_Directory_Module/src/components/ProviderCard.tsx
+++ Learning_Support_Provider_Directory_Module/src/components/ProviderCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Star, MapPin, Mail, Phone } from 'lucide-react';
 import type { Provider } from '../services/providerService';
 import { Link } from 'react-router-dom';
